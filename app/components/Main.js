e898dd638bb19b7e66cc03b94b549b9cb8ee97a9
Emily Turner-Sherwood <emilyturner@Emilys-MacBook-Pro.local>
2017-03-01 16:38:59 -0600

    setting up main file for react

--- app/components/Main.js
+++ app/components/Main.js
@@ -5,3 +5,43 @@ var React = require("react");
 var Search = require("./Search");
 
 // The Main component (The parent)
+var Main = React.createClass({
+    // Describing the component's render method
+    render: function() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="jumbotron">
+                        <h1 className="heading-display">New York Times - Article Scrubber</h1>
+                        <p className="subheading-display">Search for and save articles of interest!</p>
+                    </div>
+                    <div className="col-md-12">
+                        <div className="panel panel-default">
+                            <div className="panel-heading">
+                                <h3 className="panel-title text-center">Search</h3>
+                            </div>
+                            <div className="panel-body">
+                                <form>
+                                    <div className="form-group">
+                                        <label>
+                                            Topic:
+                                            <input type="text" className="form-control" id="searchInput" placeholder="Search articles" />
+                                        </label>
+                                        <label>
+                                            Start Year:
+                                            <input type="number" className="form-control" id="startYearInput" placeholder="Start year" />
+                                        </label>
+                                        <label>
+                                            End Year:
+                                            <input type="number" className="form-control" id="endYearInput" placeholder="End year" />
+                                        </label>
+                                        <button type="button" className="btn btn-success" onClick={this.handleClick}>Search</button>
+
+              </div>
+                </div>
+            </div>
+        )
+    }
+})
+
+module.exports = Main;
--- app/config.js
+++ app/config.js
@@ -4,6 +4,8 @@ var authkey = "08c4a36d32804e9287729e50349c495d"
 
 var queryTerm = "";
 var numResults = 0;
+var startYear = 0;
+var endYear = 0;
 
 // Based on the queryTerm a queryURL is created
 var queryURLBase = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + authkey + "&q=";
