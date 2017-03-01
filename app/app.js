f8712150d3616470fe8c9c65696fb7aec0be2c6b
Emily Turner-Sherwood <emilyturner@Emilys-MacBook-Pro.local>
2017-03-01 14:35:19 -0600

    setting up main react

--- app/app.js
+++ app/app.js
@@ -1,3 +1,10 @@
 // Including the Main React Dependencies
 var React = require("react");
 var ReactDOM = require("react-dom");
+
+// Including the main parent component, holds the main layout and navigation...
+var Main = require("./components/Main");
+
+// Will render the main component, in this case, Main.js
+ReactDOM.render(<Main />, document.getElementById("app"));
+
---
+++ app/components/Main.js
@@ -0,0 +1,7 @@
+// Including React
+var React = require("react");
+
+// Including sub-components
+var Search = require("./Search");
+
+// The Main component (The parent)
