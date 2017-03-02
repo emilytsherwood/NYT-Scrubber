d6b92c2155b722555445e678a9e2457f230360d5
Emily Turner-Sherwood <emilyturner@Emilys-MacBook-Pro.local>
2017-03-02 11:58:32 -0600

    helper completed

---
+++ app/components/children/Search.js
@@ -0,0 +1,34 @@
+// Including React
+var React = require("react");
+
+// Including sub-components
+var Saved = require("./Saved");
+
+// The Main component (The parent)
+var Search = React.createClass({
+    // Describing the component's render method
+    render: function() {
+        return (
+            <form>
+                {/*<div className="form-group">*/}
+                    <label>
+                        Topic:
+                        <input type="text" className="form-control" id="searchInput" placeholder="Search articles" />
+                    </label>
+                    <br />
+                    <label>
+                        Start Year:
+                        <input type="number" className="form-control" id="startYearInput" placeholder="Start year" />
+                    </label>
+                    <br />
+                    <label>
+                        End Year:
+                        <input type="number" className="form-control" id="endYearInput" placeholder="End year" />
+                    </label>
+                        <button type="button" className="btn btn-success" onClick={this.handleClick}>Search</button>
+                </form>
+        )
+    }
+});
+                         
+        
---
+++ app/components/utils/helper.js
@@ -0,0 +1,39 @@
+// Include the axios package for performing HTTP requests (promise based alternative to request)
+var axios = require("axios");
+
+// Geocoder API
+var newyorktimesAPI = "08c4a36d32804e9287729e50349c495d"
+
+// Helper functions for making API Calls
+var helper = {
+
+  // This function serves our purpose of running the query to geolocate.
+  runQuery: function(location) {
+
+    console.log(location);
+
+    // Figure out the geolocation
+    var queryURL =  "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + newyorktimesAP + "&q=";
+    return axios.get(queryURL).then(function(response) {
+      // If get get a result, return that result's formatted address property
+      if (response.data.results[0]) {
+        return response.data.results[0].formatted;
+      }
+      // If we don't get any results, return an empty string
+      return "";
+    });
+  },
+
+  // This function hits our own server to retrieve the record of query results
+  getHistory: function() {
+    return axios.get("/api");
+  },
+
+  // This function posts new searches to our database.
+  postHistory: function(location) {
+    return axios.post("/api", { location: location });
+  }
+};
+
+// We export the API helper
+module.exports = helper;
