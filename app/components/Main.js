baed014faba164a610d6079f49a2f308ef5f6b03
Emily Turner-Sherwood <emilyturner@Emilys-MacBook-Pro.local>
2017-03-03 15:38:58 -0600

    errors everywhere

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,69 +1,13 @@
 // Including React
 var React = require("react");
+var Router = require("react-router");
 
 // Including sub-components
 var Search = require("./children/Search");
 
-// Helper for making AJAX requests to our API
-var helpers = require("./utils/helper");
 
 // The Main component (The parent)
 var Main = React.createClass({
-
-    getInitialState: function() {
-        return {
-            searchArticles: "",
-            startYear: "",
-            endYear: "",
-            results: "",
-            duplicates: false,
-            resultsHistory: []
-        }
-    },
-
-     // The moment the page renders get the History
-  componentDidMount: function() {
-    // Get the latest history.
-    helpers.getHistory().then(function(response) {
-      console.log(response);
-      if (response !== this.state.resultsHistory) {
-        console.log("History", response.data);
-        this.setState({ resultsHistory: response.data });
-      }
-    }.bind(this));
-  },
-
-  // If the component changes (i.e. if a search is entered)...
-  componentDidUpdate: function() {
-
-    // Run the query for the address
-    helpers.runQuery(this.state.searchArticles).then(function(data) {
-      if (data !== this.state.results) {
-        console.log("Address", data);
-        this.setState({ results: data });
-
-    // After we've received the result... then post the search term to our history.
-    helpers.postHistory(this.state.searchArticles).then(function() {
-        console.log("Updated!");
-
-    // After we've done the post... then get the updated history
-    helpers.getHistory().then(function(response) {
-        console.log("Current History", response.data);
-
-        console.log("History", response.data);
-
-            this.setState({ resultsHistory: response.data });
-
-          }.bind(this));
-        }.bind(this));
-      }
-    }.bind(this));
-  },
-
-  // This function allows childrens to update the parent.
-  setTerm: function(term) {
-    this.setState({ searchArticles: term });
-  },
 
     // Describing the component's render method
     render: function() {
--- app/components/children/Search.js
+++ app/components/children/Search.js
@@ -1,12 +1,66 @@
 // Including React
 var React = require("react");
 var axios = require("axios");
+var Router = require("react-router");
+
+// Helper for making AJAX requests to our API
+var helper = require("../utils/helpers");
 
 // Including sub-components
 var Saved = require("./Saved.js");
 
 // The Main component (The parent)
 var Search = React.createClass({
+
+        getInitialState: function() {
+        return {
+            results: []
+        }
+    },
+
+     // The moment the page renders get the History
+  componentDidMount: function() {
+    // Get the latest history.
+    helper.getArticles().then(function(response) {
+      console.log(response);
+      if (response !== this.state.results) {
+        console.log("Articles", response.data);
+        this.setState({ results: response.data });
+      }
+    }.bind(this));
+  },
+
+  // If the component changes (i.e. if a search is entered)...
+//   componentDidUpdate: function() {
+
+//     // Run the query for the address
+//     helpers.runQuery(this.state.searchArticles).then(function(data) {
+//       if (data !== this.state.results) {
+//         console.log("Address", data);
+//         this.setState({ results: data });
+
+//     // After we've received the result... then post the search term to our history.
+//     helpers.postHistory(this.state.searchArticles).then(function() {
+//         console.log("Updated!");
+
+//     // After we've done the post... then get the updated history
+//     helpers.getHistory().then(function(response) {
+//         console.log("Current History", response.data);
+
+//         console.log("History", response.data);
+
+//             this.setState({ resultsHistory: response.data });
+
+//           }.bind(this));
+//         }.bind(this));
+//       }
+//     }.bind(this));
+//   },
+
+//   // This function allows childrens to update the parent.
+//   setTerm: function(term) {
+//     this.setState({ searchArticles: term });
+//   },
     // Describing the component's render method
     render: function() {
         return (
